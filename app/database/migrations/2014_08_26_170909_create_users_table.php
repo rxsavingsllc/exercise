1f8e8ed610b0d12c987ed026dbeff70178e315a0
lxndrtsh <lxndr.tsh@gmail.com>
2014-09-03 13:47:47 -0500

    Slight Modification
    Changed the migration to make the secret key unique.

--- app/database/migrations/2014_08_26_170909_create_users_table.php
+++ app/database/migrations/2014_08_26_170909_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-            $table->string('secret_key');
+            $table->string('secret_key')->unique();
             $table->string('first_name');
             $table->string('last_name');
             $table->timestamps();
